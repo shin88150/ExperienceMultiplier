1ab87583b0c85157bef9f29e9e01a1230e53f6f3
shin88150 <98669598+shin88150@users.noreply.github.com>
2022-01-29 23:01:25 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -18,13 +18,13 @@ namespace ExperienceMultiplier
 {
     public class MainConfig
     {
-        public int GeneralMultiplier { get; set; } = 1;
-        public int CombatMultiplier { get; set; } = 1;
-        public int FarmingMultiplier { get; set; } = 1;
-        public int FishingMultiplier { get; set; } = 1;
-        public int MiningMultiplier { get; set; } = 1;
-        public int ForagingMultiplier { get; set; } = 1;
-        public int LuckMultiplier { get; set; } = 1;
+        public int GeneralMultiplier { get; set; } = 15;
+        public int CombatMultiplier { get; set; } = 15;
+        public int FarmingMultiplier { get; set; } = 15;
+        public int FishingMultiplier { get; set; } = 15;
+        public int MiningMultiplier { get; set; } = 15;
+        public int ForagingMultiplier { get; set; } = 15;
+        public int LuckMultiplier { get; set; } = 15;
     }
 
     public class Main : Mod
